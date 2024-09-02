cedef14c0b9fa42da1b7d52c38d819f994d6eb6c
ulisserax <ulissesbgd@gmail.com>
2024-09-02 18:01:19 -0300

    adding longestCommonPrefix exercise

---
+++ LongestCommonPrefix/lngstComPrfx.js
@@ -0,0 +1,13 @@
+function longestCommonPrefix(strs) {
+    if (strs.length === 0) {
+        return '';
+    }
+    var prefix = strs[0];
+    for (var i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.substring(0, prefix.length - 1);
+        }
+    }
+    return prefix;
+}
+console.log(longestCommonPrefix(["dog", "doggy", "doggystyle"])); // fl
